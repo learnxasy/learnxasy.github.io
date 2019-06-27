f1768db1cc0827586818aeb049dfbbfe4eb8c670
Shawn Lee <shawnlee@sleepymac.local>
2019-06-27 00:10:07 -0700

    Add deploy script

--- src/pages/learn.js
+++ src/pages/learn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 export default ({ data: { allMarkdownRemark: { edges: subjects } } }) => (
   <Layout>
